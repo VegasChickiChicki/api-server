e13314589171b552938c91b9fe99d9cda09d788a
LasVegas <dodaaa@bk.ru>
2020-10-17 20:17:59 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Build a server with Node's HTTP module
+const express = require('express');
+const BodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(BodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(BodyParser.json());
+
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(8080, () => console.log('Example app listening on port 8080'));
+
+
+app.post('/', (request, response) => {
+  console.clear();
+  console.log(request.body);
+
+  next();
+});
