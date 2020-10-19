53b1054bbba2aaa5a070e8d0dac3d6bf5f2d7c90
LasVegas <dodaaa@bk.ru>
2020-10-19 12:16:22 +0300

    v 1.1.0 проверка работы сокетов

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const express = require('express');
+const app = require('express')();
+const http = require('http').createServer(app);
+const { MongoClient } = require("mongodb");
 const BodyParser = require('body-parser');
+const cors = require('cors');
+const io = require('socket.io')(http);
 
-const app = express();
-
-app.use(BodyParser.urlencoded({
-  extended: true,
-}));
-
+app.use(cors());
 app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({extended: true}));
 
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('NewMessage', msg => {
+    io.emit('update-chat', msg);
+  });
+});
 
 app.get('/', (req, res) => res.send('api-server is ready to work!'));
 
-app.listen(8080, () => console.log('Example app listening on port 8080'));
-
-
-app.post('/', (request, response, next) => {
-  console.clear();
-  console.log(request.body);
-
-  response.json({status: true});
-
-  next();
-});
+http.listen(8080, () => console.log('Example app listening on port 8080'));
