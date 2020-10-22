9dca3d28229cf993902758867025e449afa24269
LasVegas <dodaaa@bk.ru>
2020-10-22 03:50:07 +0300

    Изменил адрес до бд. Поменял версию 1.3.8

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: 8080,
   //MongoUrl: 'mongodb://127.0.0.1:27017/nuxt-chat',
-  MongoUrl: 'mongodb://127.0.0.1:27017:2020/nuxt-chat',
+  MongoUrl: 'mongodb://127.0.0.1:27017/nuxt-chat',
 };
