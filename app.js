293848e545672f3ef0331bb1142c8aeaf208630e
LasVegas <dodaaa@bk.ru>
2020-11-02 02:59:34 +0300

    Поменял версию - 2.0.3

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/api/chat/options', routes.chat.options.join);
 
 
 app.get('/', (req, res) => {
-  res.send('api-server is ready to work! version - 2.0.2')
+  res.send('api-server is ready to work! version - 2.0.3')
 });
 
 module.exports = require('http').createServer(app);
